8d63599326225ed1f89b778c52229716e64a2cfc
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-06-17 14:31:14 +0200

    First commit

---
+++ MailmotorBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mailmotor\Bundle\MailmotorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Mailmotor bundle
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class MailmotorBundle extends Bundle
+{
+}
