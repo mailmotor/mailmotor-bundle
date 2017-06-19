08905fc40f6772d1f1b11f9bedfa973c6875f59c
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-19 16:42:35 +0200

    PHPDoc updated

--- Manager/SubscriberGatewayManager.php
+++ Manager/SubscriberGatewayManager.php
@@ -6,6 +6,7 @@ use MailMotor\Bundle\MailMotorBundle\Gateway\SubscriberGateway;
 
 class SubscriberGatewayManager
 {
+    /** @var array */
     private $subscriberGateways;
 
     public function __construct()
