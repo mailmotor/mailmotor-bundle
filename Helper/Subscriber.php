69d1d960c275a42ea8a9fec2a626fd5ae1a1f52a
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-20 16:33:00 +0200

    Using @param bool instead of boolean

--- Helper/Subscriber.php
+++ Helper/Subscriber.php
@@ -79,7 +79,7 @@ final class Subscriber extends MailMotor
      * @param string $language
      * @param array $mergeFields
      * @param array $interests The array is like: ['9AS489SQF' => true, '4SDF8S9DF1' => false]
-     * @param boolean $doubleOptin Members need to validate their emailAddress before they get added to the list
+     * @param bool $doubleOptin Members need to validate their emailAddress before they get added to the list
      * @param string $listId
      * @return boolean
      */
